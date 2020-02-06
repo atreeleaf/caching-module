03b588382e6b67684ec47773e8442fcee2032cf9
Andrew Tang <atreeleaf@gmail.com>
2020-02-06 12:33:34 -0800

    created sandbox to test caching.service.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import CachingService from './services/caching.service';
+import redis from 'redis';
+
+const client = redis.createClient();
+
+const mockCachingService = new CachingService(client, 'SHA256');
+
+const hash = mockCachingService.generateHash('hello');
+
+console.log(hash);
+
+const writeCache = await mockCachingService.writeCache(hash, 'hello');
+const result = await mockCachingService.readCache(hash);
+console.log(result);
 
 export default CachingService;
